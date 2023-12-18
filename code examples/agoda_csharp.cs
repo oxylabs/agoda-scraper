f9891bee6835332a4da375dd8be8e2ba6261959f
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 13:19:11 +0200

    Adding code examples/agoda_csharp.cs

---
+++ code examples/agoda_csharp.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace OxyApi
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            const string Username = "YOUR_USERNAME";
+            const string Password = "YOUR_PASSWORD";
+
+            var parameters = new Dictionary<string, string>()
+            {
+                { "source", "universal" },
+                { "url", "https://www.agoda.com/?cid=1891458&tag=24bd4b3f-b6a1-50d5-e639-d9c49ca41c49&gclid=cj0kcqia7oqrbhd9arisak3uxh0kts4hidqpr1aegsv5v_2puktdlkqbnwfahx2kuewg9gmwkerfz_oaalcyealw_wcb&ds=d0lupzqbrarv6xh%2f"},
+            }
+
+
+            var client = new HttpClient();
+
+            Uri baseUri = new Uri("https://realtime.oxylabs.io");
+            client.BaseAddress = baseUri;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, "/v1/queries");
+            requestMessage.Content = JsonContent.Create(parameters);
+
+            var authenticationString = $"{Username}:{Password}";
+            var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.ASCIIEncoding.UTF8.GetBytes(authenticationString));
+            requestMessage.Headers.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
+
+            var response = await client.SendAsync(requestMessage);
+            var contents = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(contents);
+        }
+    }
+}
