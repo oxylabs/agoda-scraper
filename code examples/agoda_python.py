f347d2a24f15b910bbff0046ad0e28b4c6cc6766
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 13:19:16 +0200

    Adding code examples/agoda_python.py

---
+++ code examples/agoda_python.py
@@ -0,0 +1,20 @@
+import requests
+from pprint import pprint
+
+# Structure payload.
+payload = {
+    'source': 'universal',
+    'url': 'https://www.agoda.com/?cid=1891458&tag=24bd4b3f-b6a1-50d5-e639-d9c49ca41c49&gclid=cj0kcqia7oqrbhd9arisak3uxh0kts4hidqpr1aegsv5v_2puktdlkqbnwfahx2kuewg9gmwkerfz_oaalcyealw_wcb&ds=d0lupzqbrarv6xh%2f'
+}
+
+# Get response.
+response = requests.request(
+    'POST',
+    'https://realtime.oxylabs.io/v1/queries',
+    auth=('user', 'pass1'),
+    json=payload,
+)
+
+# Instead of response with job status and results url, this will return the
+# JSON response with the result.
+pprint(response.json())
